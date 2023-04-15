7dc54e31ced56a60838b8033ca745456373747b7
Michał Grzegorczyk <51246162+DMFN-code@users.noreply.github.com>
2023-04-16 01:51:13 +0200

    Update parallel_div.c
    properly working now, with minor fixes and refactorings here and there ;)

--- parallel_div.c
+++ parallel_div.c
@@ -1,78 +1,73 @@
-#include<stdio.h>
-#include<math.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<math.h>
-#include<omp.h>
-#define shortlist
-// #define verboselist
-//#define wer2
-#define wer1
-int main(int argc,char* argv[]){
-    int min =2;
-    int max=10000001;
-    int size=max-min;
-    bool prime=false;
-    int  *primes;
-    primes=malloc(sizeof(int)*size);
-    int num_primes=1;
-    
-    int cur=1;
-    primes[0]=2;
-    #pragma omp parallel shared(num_primes)
-    {
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <omp.h>
+#include <math.h>
+#include <time.h>
 
-      #pragma omp for schedule(guided,3) reduction(+:num_primes)
-        for(int i=min;i<=max;i++){
-        
-        int loc_max=sqrt(i)+1;
-        bool prime=false;
-        #ifdef wer1 
-        for(int j=2;j<=loc_max;j++){
-            
-            if(i%j==0){
-                prime=false;
-                break; 
-            }else{
-                prime=true;
-         }   
+#define verboselist
+// #define savelist
+
+int main(int argc, char* argv[]){
+    clock_t cstart, cend;
+    double start, end;
+    int min = 2;
+    int max = 1000000;
+    int size = max + 1 - min;
+
+    int *primes;
+    primes = malloc(sizeof(int) * size);
+    int num_primes = 0;
+
+    if(min <= 2)
+        primes[num_primes++] = 2;
+
+    bool prime;
+
+    cstart = clock();
+    start = omp_get_wtime();
+
+    #pragma omp parallel shared(num_primes) private(prime)
+    #pragma omp for schedule(guided)
+    for(int i = min; i <= max; i++){
+        int loc_max = (int) ceil(sqrt(i));
+        prime = true;
+
+        for(int j = 2; j <= loc_max; j++){
+            if(i % j == 0){
+                prime = false;
+                break;
+            }
         }
-        #endif
-        #ifdef wer2
-        for(int y=0;y<num_primes;y++){
-              if(i%primes[y]==0){
-                prime=false;
-                break; 
-            }else{
-                prime=true;
-         }   
-        }
-        #endif
-        if(prime){
-        
-        primes[num_primes]=i;
-       
-        num_primes++;
-        
-    }
-    }
+
+        #pragma omp critical (num_primes) 
+        if(prime)
+            primes[num_primes++] = i;
     } 
-    #ifdef verboselist
-    for (int i=0;i<num_primes;i++){
-        printf("%d ",primes[i]);
-    }
-    #endif
-      
-    //   FILE * f=fopen("seq_div.txt","w");
-    //   fclose(f);
-    //   for (int i=0;i<num_primes;i++){
-    //   f=fopen("seq_div.txt","a");
-    //   fprintf(f,"%d\n",primes[i]);
-    //   fclose(f);
-    //   }
-      free(primes);
-#ifdef shortlist
-printf("W zakresie od %d do %d wykryto %d liczb pierwszych \n",min,max,num_primes);
+    
+    cend = clock();
+    end = omp_get_wtime();
 
-#endif
+    #ifdef verboselist
+    for (int i; i < num_primes; i++){
+        printf("%-8d", primes[i]);
+        if((i + 1) % 10 == 0)
+            printf("\n");
+    }
+    printf("\n");
+    #endif
+
+    #ifdef savelist
+    FILE *f = fopen("parallel_div.txt","w+");
+    for (int i = 0; i < num_primes; i++){
+        fprintf(f,"%d\n",primes[i]);
+    }
+    fclose(f);
+    #endif
+
+    free(primes);
+
+    printf("\nCzas procesora: %fs \nCzas przetwarzania: %fs\n%d liczb pierwszych\n", (double)(cend - cstart)/CLOCKS_PER_SEC, end - start, num_primes);
+
+    return EXIT_SUCCESS;
 }
