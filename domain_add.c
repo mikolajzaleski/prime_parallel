3333f5da1ba4f6c1e02b93af68b434081629f839
Mikolaj Zaleski <mikolajzaleski@outlook.com>
2023-05-09 21:22:54 +0200

    Added function to create starting primes

--- domain_add.c
+++ domain_add.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include<unistd.h>
+
+#include <unistd.h>
 #include <math.h>
-#include<string.h>
-#include<omp.h>
-#include<time.h>
-#include<fcntl.h>
+#include <string.h>
+#include <omp.h>
+#include <time.h>
+#include <fcntl.h>
+#define START_NUMBER 10000
+#define PARTITION_SIZE sizeof(bool)*(max-min/8+1)
+
+unsigned int* create_start_primes(unsigned int min,unsigned  int max);
 int main(int argc, char *argv[])
 {
     clock_t cstart, cend;
@@ -15,32 +19,28 @@ int main(int argc, char *argv[])
     unsigned int min = 2;
     unsigned int max = 300000000;
     unsigned int size = max - min;
-    FILE f;
+    unsigned int* start_p=create_start_primes(3,10000);
+    for(unsigned int i=1;i<start_p[0];i++){
+        printf("%d ",start_p[i]);
+    }
     bool prime = false;
     int *primes;
     primes = malloc(sizeof(int) * size);
     int num_primes = 0;
     bool * primes_bool;
-    primes_bool=malloc(sizeof(bool)*(max + 1));
-    //memset(primes_bool, true, sizeof(primes_bool))false;
-    bool *subset1;
-    bool *subset2;
-    bool *subset3;
-    bool *subset4;
-    bool *subset5;
-    bool *subset6;
-    bool *subset7;
-    bool *subset8;
-    subset1=malloc(sizeof(bool)*(max/8+1));
-    subset2=malloc(sizeof(bool)*(max/8+1));
-    subset3=malloc(sizeof(bool)*(max/8+1));
-    subset4=malloc(sizeof(bool)*(max/8+1));
-    subset5=malloc(sizeof(bool)*(max/8+1));
-    subset6=malloc(sizeof(bool)*(max/8+1));
-    subset7=malloc(sizeof(bool)*(max/8+1));
-    subset8=malloc(sizeof(bool)*(max/8+1));
+    primes_bool = malloc(sizeof(bool)*(max-min + 1));
+    
+    bool *subset1, *subset2, *subset3, *subset4, *subset5, *subset6, *subset7, *subset8;
+    subset1=malloc(PARTITION_SIZE);
+    subset2=malloc(PARTITION_SIZE);
+    subset3=malloc(PARTITION_SIZE);
+    subset4=malloc(PARTITION_SIZE);
+    subset5=malloc(PARTITION_SIZE);
+    subset6=malloc(PARTITION_SIZE);
+    subset7=malloc(PARTITION_SIZE);
+    subset8=malloc(PARTITION_SIZE);
     bool *subsets[]={subset1,subset2,subset3,subset4,subset5,subset6,subset7,subset8};
-    subsets[0][1]=1;
+    
 
     int sqrt_sieve = sqrt(max);
     int tmp; // temporary sum
@@ -49,31 +49,25 @@ int main(int argc, char *argv[])
     start = omp_get_wtime();
     
 
-    #pragma omp parallel shared(i) 
-    {
+    #pragma omp parallel shared(i)
     for ( i = 2; i <= sqrt_sieve; i++)
     {
         if (primes_bool[i] == false)
         {
             #pragma omp parallel for schedule(guided,1) 
-
             for (tmp = i+i; tmp < max; tmp += (i))
             {
                 primes_bool[tmp]=true;
-                
-             
             }
         }
-    }
     }
     
     cend = clock();
     end = omp_get_wtime();
-    for (int i=2;i<max;i++){
+    for (int i=2;i<max;i++)
         if(!primes_bool[i]) num_primes+=1;
 
-    }
-    printf("%d\n",num_primes);
+    printf("\n%d\n",num_primes);
         printf("\nCzas procesora: %fs \nCzas przetwarzania: %fs\n%d liczb pierwszych\n", (double)(cend - cstart)/CLOCKS_PER_SEC, end - start, num_primes);
 
     free(primes_bool);
@@ -85,5 +79,42 @@ int main(int argc, char *argv[])
     free(subset6);
     free(subset7);
     free(subset8);
+
+    return(EXIT_SUCCESS);
+}
+unsigned int* create_start_primes(unsigned int min,unsigned int max){
+    unsigned int* start_primes;
+    int num_primes=10;
     
+    int idx=0;
+    start_primes=malloc(sizeof(int)*num_primes);
+    
+    int max_root=(int)sqrt(max);
+    bool* is_prime=malloc(sizeof(bool)*START_NUMBER);
+    for (int i=2;i<=max;i++){
+        is_prime[i-2]=1;
+    
+    }
+    for (int d=2;d<=max_root;d++){
+        if(is_prime[d-2]==0)
+            continue;
+        for(int m=d+d;m<=max;m+=d){
+            is_prime[m-2]=0;
+        }
+
+    }
+    for (int i=min-2;i<max;i++){
+        if(is_prime[i]==1)
+            {   idx++;
+                num_primes++;
+                start_primes=(unsigned int*)realloc(start_primes,sizeof(unsigned int)*num_primes);
+                start_primes[idx]=i+2;
+
+            }
+            
+    }
+    start_primes[0]=idx+1;//size of array is stored  here
+    free(is_prime);
+    return start_primes;
+
 }
